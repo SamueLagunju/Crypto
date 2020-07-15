ef4484105ba6a56fd0af4690cb3a6b434e3d91bb
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-15 05:08:59 -0400

    OS detection complete

--- cryptomagic/__main__.py
+++ cryptomagic/__main__.py
@@ -1,10 +1,23 @@
 from sys import platform as _platform
 
-print("Verifying OS...")
 
-if _platform == "linux":
-    print('Linux OS detected')
-elif _platform == "darwin":
-    print('Mac OS detected')
-elif _platform == "Windows":
-    print('Windows OS detected')
+def osCheck():
+    if _platform == "linux":
+        print('Linux OS detected')
+    elif _platform == "darwin":
+        print('Mac OS detected')
+    elif _platform == "Windows":
+        print('Windows OS detected')
+    else:
+        return False
+
+
+def main():
+    # OS Check
+    print("Verifying OS...")
+    if not osCheck():
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
