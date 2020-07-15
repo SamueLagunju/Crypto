f3c23b8701ee2a7ec9d2c4e535dcdb16aa4a560a
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-15 04:47:17 -0400

    Sample OS detection

--- cryptomagic/__main__.py
+++ cryptomagic/__main__.py
@@ -1,1 +1,10 @@
-print("____-menu_____")
+from sys import platform as _platform
+
+print("Verifying OS...")
+
+if _platform == "linux":
+    print('Linux OS detected')
+elif _platform == "darwin":
+    print('Mac OS detected')
+elif _platform == "Windows":
+    print('Windows OS detected')
