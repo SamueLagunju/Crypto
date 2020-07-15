bbbb712042a1f025260d5513b1c346a8fb585869
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-15 04:39:34 -0400

    Back to default..Added a trick from https://www.geeksforgeeks.org/usage-of-__main__-py-in-python/

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='cryptomagic',
     version='0.1.0',
     description='Sample package for Python-Guide.org',
     long_description=readme,
--- src/core.py
+++ src/core.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from . import helpers
+from src import helpers
+
 
 def get_hmm():
     """Get a thought."""
