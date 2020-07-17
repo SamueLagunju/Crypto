05967ebbbeef6559bbc2e6e035e673aa10c931b8
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-16 23:07:27 -0400

    arg_parser function basic.

--- cryptomagic/__main__.py
+++ cryptomagic/__main__.py
@@ -2,12 +2,18 @@ import platform
 
 
 def osCheck():
-    if platform.system() == "linux":
-        print('Linux OS detected')
-    elif platform.system() == "darwin":
-        print('Mac OS detected')
-    elif platform.system() == "Windows":
-        print('Windows OS detected')
+    plt = platform.system()
+    if plt == "Windows":
+        print("Your system is Windows")
+        # do x y z
+    elif plt == "Linux":
+        print("Your system is Linux")
+        # do x y z
+    elif plt == "Darwin":
+        print("Your system is MacOS")
+        # do x y z
+    else:
+        print("Unidentified system")
 
 
 def arg_parser():
