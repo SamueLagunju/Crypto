f782aab106b5a0d67b1421844acb413e56befff6
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-17 00:38:49 -0400

    Simple Arg Parsing works with help description

--- cryptomagic/__main__.py
+++ cryptomagic/__main__.py
@@ -39,13 +39,11 @@ def arg_parser(args=sys.argv[1:]):
     #     print(f'Decryption File: {args.decrypt}')
     # else:
     #     print("Default Mode: Encryption.")
-    parser = argparse.ArgumentParser(description="An encrypting / decrypting utility for Linux.")
-    parser.add_argument('-e', '--encrypt', dest='encrypt_file',
-                        help="Produces an encrypted file")
-    parser.add_argument('-d', '--decrypt',  dest='decrypt_file',
-                        help="Produces a decrypted file")
-    parser.add_argument("-n", "--number", type=int, help="A number.")
-
+    parser = argparse.ArgumentParser(description="An encrypting / decrypting utility for Linux.\n")
+    parser.add_argument('-e', '--encrypt',  metavar='Encryption file',
+                        dest='encrypt_file', help="Produces an encrypted file\n")
+    parser.add_argument('-d', '--decrypt', metavar='Decryption file',
+                        dest='decrypt_file', help="Produces a decrypted file\n")
 
     # parser.add_argument('-e', '--encrypt', action='store_true',
     #                     help="Produces an encrypted file", dest='encryption_file')
@@ -63,7 +61,7 @@ def main():
     # Cmd line parse
     options = arg_parser(sys.argv[1:])
     print(f'Encryption File: {options.encrypt_file}')
-    print(f'Encryption File: {options.decrypt_file}')
+    print(f'Decryption File: {options.decrypt_file}')
 
 
 if __name__ == "__main__":
