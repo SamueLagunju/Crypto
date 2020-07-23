d4031a39484a49fc6684fd28486668958fe4df5a
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-23 03:16:36 -0400

    Should work on linux now

--- crypto/crypto.py
+++ crypto/crypto.py
@@ -1,15 +1,9 @@
-# -*- coding: utf-8 -*-
-#
-# FILE:         crypto.py
-# PROJECT:      crypto
-# PROGRAMMER:   Samuel Lagunju
-# DESCRIPTION:  Contains the function which is the main entry point to our application.
-#
-
-__version__ = "1.0.0"
-
+import platform
+import os
 import sys
+from .constants import *
+import argparse
 
 
 def main():
-    print("I'm here")
+    print('here')
