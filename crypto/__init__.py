78b35ec5b220b0bb6c96c9e2b111fe58a6fc7c77
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-08-06 19:08:18 -0400

    File structure is complete. Moving onto other branches

--- crypto/__init__.py
+++ crypto/__init__.py
@@ -5,5 +5,9 @@
 # DESCRIPTION:  This file makes the crypto directory a package.
 #
 
+
 __title__ = 'crypto'
 __version__ = '1.0.0'
+
+
+
--- crypto/__main__.py
+++ crypto/__main__.py
@@ -1,13 +1,43 @@
-# -*- coding: utf-8 -*-
-
 #
 # FILE:         __main__.py
 # PROJECT:      crypto
 # PROGRAMMER:   Samuel Lagunju
-# DESCRIPTION:  Makes the crypto directory executable as a script.
+# DESCRIPTION:  Runs the application...
 #
 
+from .helpers import *
+from .crypter import *
+from .strategy import SeanStrategy
+from .fileio import read_file
 
-from .crypto import main
 
-main()
+def main():
+    program_directory = Path().resolve()
+    program_os = os_checker()
+    if not program_os:
+        sys.exit(SYS_ERROR)
+
+    args = arg_parser(sys.argv[1:])
+    if args.decrypt_file:
+        for file_name in args.decrypt_file:
+            print("Decrypting file: {0}".format(file_name))
+
+            crypter = Crypter(file_name, SeanStrategy())
+            file_contents = read_file(file_name)
+
+            decrypted_text = crypter.decrypt_txt(file_contents)
+
+            print(decrypted_text)
+    if args.encrypt_file:
+        for file_name in args.encrypt_file:
+            print("Encrypting file: {0}".format(file_name))
+
+            crypter = Crypter(file_name, SeanStrategy())
+            file_contents = read_file(file_name)
+
+            encrypted_text = crypter.encrypt_txt(file_contents)
+
+            print(encrypted_text)
+
+if __name__ == '__main__':
+    main()
